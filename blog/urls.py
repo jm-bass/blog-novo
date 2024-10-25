3eb3ed53d0d1c7c9681d3c888e2fbd1934db99c2
JM-Bass <joaomariapereire@gmail.com>
2024-10-25 10:19:33 -0300

    Commit fim de aula

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('post/<int:post_id>', views.post, name='post'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
     path('', views.index, name='index')
 ]
