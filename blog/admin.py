0c4b95a2901693a648c6465276d9d6a819fdd6e4
JM-Bass <joaomariapereire@gmail.com>
2024-11-01 10:22:13 -0300

    Formulário

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Blog, Mensagem
 
 admin.site.register(Post)
+admin.site.register(Blog)
+admin.site.register(Mensagem)
